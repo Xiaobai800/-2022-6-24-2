1e8a213b82835983b01b71158c5a64f517acdd9e
Xiaobai800 <2533760032@qq.com>
2022-06-24 22:50:03 +0800

    添加项目文件。

---
+++ 练习2022 6 24  2/源.cpp
@@ -0,0 +1,40 @@
+//#include<stdio.h>//循环语句
+//int main()
+//{
+//	int line = 0;
+//	printf("考研名校\n");
+//	while (line < 20000)
+//	{
+//		printf("学习加1：%d\n", line);
+//		line++;
+//	}
+//	if (line >= 20000)
+//		printf("考上研\n");
+//	return 0;
+//}
+// 
+// 函数
+//#include<stdio.h>
+//int Add(int x, int y)
+//{
+//	int z= x + y;
+//	return z;
+//}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	int sum = 0;
+//	
+//	sum = Add(num1, num2);
+//	
+//	printf("sum=%d\n", sum);
+//
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+
+	return 0;
+}
